b36bc3dde25b1d575e3c6b1c7c64dba1ee4a2cb7
Alexandr Shevtsov <ashevtsov@luxoft.com>
2019-08-28 20:25:01 +0300

    actuator added

--- src/main/java/com/example/photoapp/api/gateway/security/WebSecurity.java
+++ src/main/java/com/example/photoapp/api/gateway/security/WebSecurity.java
@@ -24,6 +24,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()
+                    .antMatchers(environment.getProperty("api.users.actuator.url.path")).permitAll()
+                    .antMatchers(environment.getProperty("api.zuul.actuator.url.path")).permitAll()
                     .antMatchers(environment.getProperty("api.h2console.url.path")).permitAll()
                     .antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url.path")).permitAll()
                     .antMatchers(HttpMethod.POST, environment.getProperty("api.login.url.path")).permitAll()
